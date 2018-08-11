ad9d2dd977089aa4cc9b0ec0b82b683cb8af158c
infodaisyn <infodaisyn@gmail.com>
2018-08-11 21:52:24 +0600

    state example

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,9 +4,18 @@ import Footer from './footer';
 import '../App.scss';
 
 class Layout extends Component { 
+  constructor () {
+      super();
+      this.state = {name:'jack'};
+  }  
+
   render() {
+    setTimeout(()=>{
+        this.setState({name:'baba'});
+    },3000)  
     return (
      <div>
+         My name is {this.state.name}
          <Header />
          <Footer />
      </div>    
